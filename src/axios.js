7ee9a3e49c6caf14dc68c1138cfacdbfd14fdaaf
sunadoi <y.sunadoi@gmail.com>
2020-04-27 07:36:28 +0900

    Router SPA

--- src/axios.js
+++ src/axios.js
@@ -6,25 +6,25 @@ const instance = axios.create({
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 
-instance.interceptors.request.use(request => {
-  console.log(request);
-  // Edit request config
-  return request;
-}, error => {
-  // リクエストのerror
-  console.log(error);
-  return Promise.reject(error);
-});
+// instance.interceptors.request.use(request => {
+//   console.log(request);
+//   // Edit request config
+//   return request;
+// }, error => {
+//   // リクエストのerror
+//   console.log(error);
+//   return Promise.reject(error);
+// });
 
-instance.interceptors.response.use(response => {
-  console.log(response);
-  // Edit request config
-  return response;
-}, error => {
-  // レスポンスのerror
-  console.log(error);
-  return Promise.reject(error);
-});
+// instance.interceptors.response.use(response => {
+//   console.log(response);
+//   // Edit request config
+//   return response;
+// }, error => {
+//   // レスポンスのerror
+//   console.log(error);
+//   return Promise.reject(error);
+// });
 
 
 export default instance;
--- src/index.js
+++ src/index.js
@@ -9,25 +9,25 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-axios.interceptors.request.use(request => {
-  console.log(request);
-  // Edit request config
-  return request;
-}, error => {
-  // リクエストのerror
-  console.log(error);
-  return Promise.reject(error);
-});
+// axios.interceptors.request.use(request => {
+//   console.log(request);
+//   // Edit request config
+//   return request;
+// }, error => {
+//   // リクエストのerror
+//   console.log(error);
+//   return Promise.reject(error);
+// });
 
-axios.interceptors.response.use(response => {
-  console.log(response);
-  // Edit request config
-  return response;
-}, error => {
-  // レスポンスのerror
-  console.log(error);
-  return Promise.reject(error);
-});
+// axios.interceptors.response.use(response => {
+//   console.log(response);
+//   // Edit request config
+//   return response;
+// }, error => {
+//   // レスポンスのerror
+//   console.log(error);
+//   return Promise.reject(error);
+// });
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
