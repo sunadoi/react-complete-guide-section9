fda77696efd3ff99a350d6f009d70057b27643ab
sunadoi <y.sunadoi@gmail.com>
2020-04-27 20:21:37 +0900

    guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    auth: false
+  }
+  
   render () {
     return (
         <div className="Blog">
@@ -32,7 +36,7 @@ class Blog extends Component {
           {/* <Route path='/' exact render={() => <h1>Home</h1>} /> */}
           {/* <Route path='/' exact render={() => <h1>Home 2</h1>} /> */}
           <Switch>
-            <Route path='/new-post' component={NewPost} />
+            { this.state.auth ? <Route path='/new-post' component={NewPost} /> : null }
             <Route path='/posts' component={Posts} />
             <Redirect from='/' to='/posts' />
             {/* <Route path='/' component={Posts} /> */}
