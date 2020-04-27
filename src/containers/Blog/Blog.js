217f4f13f10587edb27a3669054974194d490581
sunadoi <y.sunadoi@gmail.com>
2020-04-27 15:39:28 +0900

    NavLink

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
 class Blog extends Component {
   render () {
     return (
-        <div className='Blog'>
+        <div className="Blog">
           <header>
             <nav>
               <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to={{
-                  pathname: 'new-post',
+                <li><NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: '#fa923f',
+                    textDecoration: 'underline'
+                  }}>Home</NavLink></li>
+                <li><NavLink to={{
+                  pathname: '/new-post',
                   hash: '#submit',
-                  search: '?quick-submit=true'
-                }}>New Post</Link></li>
+                  search: '?quick-submit=true',
+                }}>New Post</NavLink></li>
               </ul>
             </nav>
           </header>
@@ -26,6 +34,7 @@ class Blog extends Component {
           {/* <Route path='/' exact render={() => <h1>Home 2</h1>} /> */}
           <Route path='/' exact component={Posts} />
           <Route path='/new-post' component={NewPost} />
+          <Route path='/:id' exact component={FullPost} />
         </div>
     );
   }
