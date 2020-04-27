30a29bff95a10a01f43ada266f9d0bbc8434ec2d
sunadoi <y.sunadoi@gmail.com>
2020-04-27 16:17:26 +0900

    Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -33,8 +33,10 @@ class Blog extends Component {
           {/* <Route path='/' exact render={() => <h1>Home</h1>} /> */}
           {/* <Route path='/' exact render={() => <h1>Home 2</h1>} /> */}
           <Route path='/' exact component={Posts} />
-          <Route path='/new-post' component={NewPost} />
-          <Route path='/:id' exact component={FullPost} />
+          <Switch>
+            <Route path='/new-post' component={NewPost} />
+            <Route path='/:id' exact component={FullPost} />
+          </Switch>
         </div>
     );
   }
